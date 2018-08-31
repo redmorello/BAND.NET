c29422f933584d9bc208a3d6fc0305d2d93d94fc
Ian Houghton <sales@redmorello.com>
2018-08-31 12:07:15 +0100

    Initial commit

---
+++ BAND/BandClient.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace BAND
+{
+    public class BandClient : IBandClient
+    {
+
+
+        /// <summary>
+        /// The httpclient which will be used for the api calls through the FitbitClient instance
+        /// </summary>
+        public HttpClient HttpClient { get; private set; }
+    }
+}
